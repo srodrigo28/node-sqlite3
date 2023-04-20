7999c838945a40d673955ffc477a6ce5df6a1908
Sebastiao <rodrigoexer1@gmail.com>
2023-04-19 22:46:09 -0300

    -update-create-table

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # Projeto Sqlite
-    * link.: 
-
+    * link.: https://bancodedadoscomjavascript.club.hotmart.com/lesson/V4VPVPYr72/criando-tabelas
 # Instalando dependencias
+    * npm init -y
     * npm i sqlite3
+# primeira conexao
+    const sqlite = require('sqlite3').verbose()
+
+    const db = new sqlite.Database('database.sqlite3', (err) => {
+        console.log(err, 'init' )
+    })
+# create table
+    const sqlite = require('sqlite3').verbose()
+
+    const db = new sqlite.Database('database.sqlite3', (err) => {
+        console.log(err, 'init' )
+
+        db.run(`
+            CREATE TABLE categories(
+                id INTEGER PRIMARY KEY NOT NULL,
+                category TEXT
+            );
+        `, (err) => { console.log('table create sucess!')
+        })
+    })
+# Out
---
+++ test-create-table.js
@@ -0,0 +1,36 @@
+const sqlite = require('sqlite3').verbose()
+
+const initDB = databaseFile => new Promise((resolve, reject) => {
+    const db = new sqlite.Database(databaseFile, (err) => {
+        if(err){ 
+            reject(err)
+        }
+        else{
+            resolve(db)
+        }
+    })
+})
+
+const run = (db, query) => new Promise((resolve, reject) => {
+    db.run(query, err => {
+        if(err){
+            reject(err)
+        }else{
+            resolve()
+        }
+    })
+})
+
+const createTable = async() => {
+    const db = await initDB('banco2.sqlite3')
+    await run(db, `
+        CREATE TABLE categories (
+            id INTEGER PRIMARY KEY NOT NULL,
+            category TEXT
+        ); `
+    )
+    console.log('Categoria table created!')
+}
+createTable().catch( err => {
+    console.log(err)
+})
